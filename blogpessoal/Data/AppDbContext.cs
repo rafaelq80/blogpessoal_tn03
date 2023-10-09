6743e3eefe02aa9dc1d71fff65f01c06723c3acf
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-10-09 14:43:17 -0300

    Deploy

--- blogpessoal/Data/AppDbContext.cs
+++ blogpessoal/Data/AppDbContext.cs
@@ -44,7 +44,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo criada. 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3,0,0));
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
@@ -57,7 +57,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
